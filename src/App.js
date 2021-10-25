0131883384a6c761ff683ef60c4bf94c15479109
agustin <agusgarcia3007@gmail.com>
2021-10-25 10:26:43 -0300

    change to windows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 
 const App = () =>{
+
+  const [data, setData] = useState({
+    city:'',
+    country:'',
+  });
+  const [fetching, setFetching] = useState(false);
+  const [result, setResult] = useState({});
+
+  
+  const {city, country} = data;
+
+  useEffect(() => {
+    const fetchApi = async () => {
+
+      if(fetching){
+        const appId = 'b27651485ea5c20cf3633dafbdfb9aa6';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
+  
+        const resp = await fetch(url);
+        const result = await resp.json();
+        console.log(result);
+      }
+     
+    }
+    fetchApi();
+  }, [fetching])
+
   return(
     <>
       <Header 
         title='React Weather App'
       />
+
+      <div className="formContainer">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form 
+                data={data}
+                setData={setData}
+                setFetching={setFetching}
+              />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
